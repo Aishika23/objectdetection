0c9cdefd53aee624dd36ffe6860e73ccd247ac61
Aishika23 <100014326+Aishika23@users.noreply.github.com>
2023-04-06 13:26:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function lo1() {
+    window.location = "livingroom.html";
+}
+function lo2() {
+    window.location = "bedroom.html";
+}
+function lo3() {
+    window.location = "hallway.html";
+}
+function lo4() {
+    window.location = "building.html";
+}
---
+++ main2.js
@@ -0,0 +1,45 @@
+img = "";
+Status = "";
+objects = [];
+percent = 0;
+
+function preload(){
+    img = loadImage('bedroom.jpg');
+  }
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocoSSD', modelLoaded);
+}
+function modelLoaded() {
+    console.log("Model Loaded");
+    Status = true;
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+         objects = results;
+    }
+}
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    if (Status != "") {
+        for (i = 0; i < objects.length; i++) 
+        {
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label+" "+percent+"%", objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y,  objects[i].height, objects[i].width);
+        }
+    }
+    
+}
